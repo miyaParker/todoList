5b6a83be807fed3420d89bf14161fdb998e32c22
geekToes <karenziboh@hotmail.com>
2020-07-10 13:06:35 +0100

    implemented local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,19 @@ export class AppComponent {
   addTodo(text: string) {
     const todo = { text, status: "pending", id: Date.now() }
     this.todoList.push(todo)
+    localStorage.setItem('task-list',JSON.stringify(this.todoList))
   }
   checkTodo(id) {
     const todoItem = this.todoList.filter(todo => todo.id === +id)
     todoItem[0].status="completed"
     const todoIndex = this.todoList.indexOf(todoItem[0])
     this.todoList.splice(todoIndex,1)
+    localStorage.setItem('task-list',JSON.stringify(this.todoList))
     this.completedTodos.push(todoItem[0])
   }
+ngOnInit(){
+  if(!!localStorage && localStorage.getItem('task-list')){
+    this.todoList= JSON.parse(localStorage.getItem('task-list'))
+  }
+}
 }
