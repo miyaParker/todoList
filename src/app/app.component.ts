907a64f7d6ddfd651b6295e6e2603c782b055195
geekToes <karenziboh@hotmail.com>
2020-07-10 07:30:09 +0100

    modified files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITask } from './task/task.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngApp';
+  title: string = 'Taskly';
+  emptyTodoList: string = 'Add a todo'
+  uncompletedTodos: string = 'Your completed tasks wil appear here'
+  todoList: ITask[] = []
+  completedTodos:ITask[]=[]
+
+  addTodo(text: string) {
+    const todo = { text, status: "pending", id: Date.now() }
+    this.todoList.push(todo)
+  }
+  checkTodo(id) {
+    const todoItem = this.todoList.filter(todo => todo.id === +id)
+    todoItem[0].status="completed"
+    const todoIndex = this.todoList.indexOf(todoItem[0])
+    this.todoList.splice(todoIndex,1)
+    this.completedTodos.push(todoItem[0])
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    InputComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
